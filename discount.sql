78dbb92a19dbb6e8dc543b7986bdd1843bf31903
NicolasNRT <156657680+NicolasNRT@users.noreply.github.com>
2024-02-05 15:32:55 +0100

    Update discount query

---
+++ discount.sql
@@ -0,0 +1,7 @@
+SELECT 
+  date_date,
+  ROUND(SUM(turnover),2) AS turnover
+  ROUND(SUM(turnover_before_promo) - SUM(turnover),2) AS discount
+FROM `data-analytics-bootcamp-363212.course14.gwz_sales` 
+GROUP BY date_date
+ORDER BY date_date
--- sales_daily.sql
+++
@@ -1,4 +0,0 @@
-SELECT 
-  date_date,
-  ROUND((turnover_before_promo-turnover),2) AS discount
-FROM `data-analytics-bootcamp-363212.course14.gwz_sales` 
