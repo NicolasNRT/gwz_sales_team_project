878a5ec50aedf2412e4b845716138f29a05b4d25
NicolasNRT <156657680+NicolasNRT@users.noreply.github.com>
2024-02-05 15:59:08 +0100

    Delete discount.sql

--- discount.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  date_date,
-  ROUND(SUM(turnover),2) AS turnover
-  ROUND(SUM(turnover_before_promo) - SUM(turnover),2) AS discount
-FROM `data-analytics-bootcamp-363212.course14.gwz_sales` 
-GROUP BY date_date
-ORDER BY date_date
